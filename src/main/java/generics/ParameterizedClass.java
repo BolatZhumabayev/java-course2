7acb057952309679868160f908f5992a841c3070
Bulat Zhumabayev <bolatzh@gmail.com>
2022-06-10 22:01:49 +0600

    add parameterized class

---
+++ src/main/java/generics/ParameterizedClass.java
@@ -0,0 +1,30 @@
+package generics;
+
+public class ParameterizedClass {
+    public static void main(String[] args) {
+        Info<String> info1 = new Info<>("Ok");
+        System.out.println(info1);
+        String s1 = info1.getValue();
+
+        Info<Integer> info2 = new Info<>(42);
+        System.out.println(info2);
+        Integer s2 = info2.getValue();
+    }
+}
+
+class Info<T> {
+    private T value;
+
+    public Info(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "{[ "  + value + " ]}";
+    }
+}
